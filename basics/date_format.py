ef9df95d2f0345db10aebb021bfcba5796a06cff
esh009-del <eshaan.mohammed19@gmail.com>
2026-01-15 12:43:04 +0000

    Add user input for date formatting with separator

--- basics/date_format.py
+++ basics/date_format.py
@@ -34,3 +34,13 @@
 # Python3
 # Keyword	| Min Count	| Max Count
 # sep	    |    1	    |    -
+
+
+
+day=int(input("What is the day today ?"))
+month=int(input("What is the month?"))
+year=int(input("What is the year?"))
+separator = input("Enter the separator you want to use")
+print("Converted result in form of date is", end=" ")
+#tells python that we want the result printed in the same line
+print(day,separator,month,separator,year )
